e0fdbdfa662c9b8d91425152843340fe9ddb6935
deriabin_85 <deriabin_85@mail.ru>
2025-04-17 14:11:51 +0300

    Add config in db.py for sync connetion to psql

--- src/core/db.py
+++ src/core/db.py
@@ -1,14 +1,18 @@
-from typing import AsyncGenerator
+from contextlib import contextmanager
+from typing import AsyncGenerator, Generator
 
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 from src.core.config import settings
 
-DATABASE_URL = settings.postgres.db_uri
+DATABASE_URL_ASYNC = settings.postgres.db_async_uri
+DATABASE_URL_SYNC = settings.postgres.db_sync_uri
 
+# async
 engine = create_async_engine(
-    DATABASE_URL,
+    DATABASE_URL_ASYNC,
     echo=settings.postgres.echo,
     pool_size=settings.postgres.pool_size,
     max_overflow=settings.postgres.max_overflow,
@@ -20,6 +24,10 @@ AsyncSessionLocal = sessionmaker(
     expire_on_commit=False,
 )
 
+# sync
+SyncEngine = create_engine(DATABASE_URL_SYNC, echo=False)
+SyncSessionLocal = sessionmaker(bind=SyncEngine, expire_on_commit=False)
+
 Base = declarative_base()
 
 
@@ -27,3 +35,17 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
     """Зависимость для FastAPI"""
     async with AsyncSessionLocal() as session:
         yield session
+
+
+@contextmanager
+def get_sync_db_session() -> Generator[Session, None, None]:
+    """Синхронная сессия для Celery."""
+    session = SyncSessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
