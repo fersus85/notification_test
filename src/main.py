9620948685dff6ed89115d7bf9df743cffe94a00
deriabin_85 <deriabin_85@mail.ru>
2025-04-18 15:17:05 +0300

    Fix main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import logging
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 
 from src.api import router as api_router
@@ -16,27 +15,10 @@ app = FastAPI(
     title="Notification API",
     description="Notification service",
     version="1.0.0",
-    # lifespan=lifespan,
     exception_handlers=exception_handlers,
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
-)
-
-
-# app.middleware("http")(log_stuff)
-# if settings.ENV == EnvMode.PROD:
-#     app.middleware("http")(limiter)
-#     app.middleware("http")(before_request)
-#     configure_tracer()
-#     FastAPIInstrumentor.instrument_app(app)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api")
