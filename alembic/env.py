550e681432c5475a85a3c2896af2043732f3ae55
deriabin_85 <deriabin_85@mail.ru>
2025-04-18 12:08:38 +0300

    Add .dockerignore

--- alembic/env.py
+++ alembic/env.py
@@ -30,7 +30,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", settings.postgres.db_uri)
+config.set_main_option("sqlalchemy.url", settings.postgres.db_async_uri)
 
 
 def run_migrations_offline() -> None:
